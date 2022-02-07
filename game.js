09fdcba95b321ea6f6192a271e1c71a0588178ff
kqwang123 <32421263+kqwang123@users.noreply.github.com>
2022-02-07 07:25:48 -0600

    Add files via upload

--- game.js
+++ game.js
@@ -1,1 +1,34 @@
-var x = 0
+
+let playerCards = [];
+let computerCards = [];
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById('draw').addEventListener('click', () => {
+        draw();
+    });
+    document.getElementById('new-game').addEventListener('click', () => {
+        startGame();
+    });
+
+    startGame();
+});
+
+/**
+ * Start the game
+ * 
+ * 
+ */
+function startGame() {
+
+
+
+}
+
+/**
+ * Draws a card
+ */
+function draw() {
+
+
+
+}
